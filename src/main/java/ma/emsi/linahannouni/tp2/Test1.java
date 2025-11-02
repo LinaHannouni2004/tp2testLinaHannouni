f25dfb1194603dbe64f33e0f0d182dda45b928fd
LinaHannouni <linahannounipro2004@gmail.com>
2025-11-02 21:00:21 +0100

    test 1

---
+++ src/main/java/ma/emsi/linahannouni/tp2/Test1.java
@@ -0,0 +1,31 @@
+package ma.emsi.linahannouni.tp2;
+
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        String cle = System.getenv("GEMINI_KEY");
+
+        //Création du Modèle
+
+        ChatModel model = GoogleAiGeminiChatModel
+                .builder()
+                .apiKey(cle)
+                .modelName("gemini-2.5-flash")
+                .temperature(0.7)
+                .logRequests(true)
+                .build();
+
+        //posez une question
+
+        String reponse = model.chat("quel est la capitale du Maroc");
+        System.out.println(reponse);
+
+
+    }
+
+
+}
