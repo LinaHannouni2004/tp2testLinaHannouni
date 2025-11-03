952fdd045eb3cf187c465fdbe830cd051c830747
LinaHannouni <linahannounipro2004@gmail.com>
2025-11-03 01:04:46 +0100

    test6 + avec les 3cas (metéo)

--- src/main/java/ma/emsi/linahannouni/tp2/Test6.java
+++ src/main/java/ma/emsi/linahannouni/tp2/Test6.java
@@ -1,7 +1,21 @@
 package ma.emsi.linahannouni.tp2;
 
 
+
+
+
 import dev.langchain4j.agent.tool.Tool;
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 import ma.emsi.linahannouni.tp2.tools.meteo.MeteoTool;
 
 import java.io.IOException;
@@ -13,10 +27,50 @@ import java.net.URI;
 public class Test6 {
 
 
+
+
     public static void main(String[] args) {
-        MeteoTool tool = new MeteoTool();
-        String resultat = tool.donneMeteo("Casablanca");
-        System.out.println(resultat);
+
+        String llmKey = System.getenv("GEMINI_KEY");
+
+        ChatModel model = GoogleAiGeminiChatModel
+                .builder()
+                .apiKey(llmKey)
+                .modelName("gemini-2.5-flash")
+                .temperature(0.3)
+                .logRequestsAndResponses(true)
+                .build();
+
+
+
+
+
+
+
+        AssitantMeteo assistant =
+                AiServices.builder(AssitantMeteo.class)
+                        .chatModel(model)
+                        .tools(new MeteoTool())  // Ajout de l'outil
+                        .build();
+
+        // 💬 Exécutions de test
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("==================================================");
+                System.out.println("Posez votre question : ");
+                String question = scanner.nextLine();
+                if (question.isBlank()) {
+                    continue;
+                }
+                System.out.println("==================================================");
+                if ("fin".equalsIgnoreCase(question)) {
+                    break;
+                }
+                String reponse = assistant.chat(question);
+                System.out.println("Assistant : " + reponse);
+                System.out.println("==================================================");
+            }
     }
 
-}
+}}
+
